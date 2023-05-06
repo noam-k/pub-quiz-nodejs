ceb54ac3cfd7ad8ca546fa336780d7d9c522e01e
Noam Kritenberg <noamk.hippocampus@gmail.com>
2023-05-06 21:57:39 +0200

    First attempt at saving request as a CSV

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const http = require('http');
+const fs = require('fs');
+const parse = require('csv-parse');
+const stringify = require('csv-stringify');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
@@ -9,16 +12,33 @@ const server = http.createServer((req, res) => {
     });
     req.on('end', () => {
       try {
+        console.log('parsing request...');
         const data = JSON.parse(body);
         const userId = data.userId;
         const qaPairs = data.qaPairs;
         console.log(`Received data from user ${userId}:`, qaPairs);
-        // Add data processing
+        // Write the data to a CSV file
+        const csvFilePath = './data.csv';
+        let fileData;
+        if (fs.existsSync(csvFilePath)) {
+          fileData = fs.readFileSync(csvFilePath, 'utf-8');
+        } else {
+          fileData = stringify([['User ID', 'Question', 'Answer']]);
+        }
+        const records = parse(fileData, { columns: true });
+        qaPairs.forEach(qaPair => {
+          const record = { 'User ID': userId, 'Question': qaPair.question, 'Answer': qaPair.answer };
+          records.push(record);
+        });
+        const newFileData = stringify(records, { header: true });
+        fs.writeFileSync(csvFilePath, newFileData);
+        // Send a response
         res.writeHead(200, { 'Content-Type': 'application/json' });
         const responseBody = { message: 'Data received and processed successfully!' };
         res.write(JSON.stringify(responseBody));
         res.end();
       } catch (error) {
+        console.log(error);
         res.writeHead(400, { 'Content-Type': 'text/plain' });
         res.end('Bad Request');
       }
